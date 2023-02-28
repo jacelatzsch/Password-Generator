df47c90a5270b9a8955070c1956df20ea9eda769
Jace Latzsch <123847352+jacelatzsch@users.noreply.github.com>
2023-02-27 23:38:13 -0600

    Add files via upload

---
+++ password-generator/Develop/script.js
@@ -0,0 +1,22 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+const generate = document.getElementById("generate");
+generate.addEventListener("click", () => {
+  const userInput = prompt("");
+})
